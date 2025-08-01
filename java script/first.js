da8eb046150c8c3f6fb02a730074824ae9c3d54c
Muhammad Farhan <farhansheikh7060@gmail.com>
2025-08-01 22:03:51 +0500

    Create first.js

---
+++ java script/first.js
@@ -0,0 +1,14 @@
+let a = 'farhan';
+let b = 6;
+console.log(a+b)
+console.log(typeof(a))
+let c={
+    name:"farhan",
+    age:23
+}
+console.log(c);
+c['name']='ali';
+console.log(c)
+c={};
+console.log(c)
+

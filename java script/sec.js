d0b9db3ad5c435b203b9debda465fcce1c3d427c
Muhammad Farhan <farhansheikh7060@gmail.com>
2025-08-02 03:05:03 +0500

    Update sec.js

--- java script/sec.js
+++ java script/sec.js
@@ -8,4 +8,16 @@ let compare = (a , b)=>{
 }
 let arr1 = [3,2,4,55,33,55,222];
 arr1.sort(compare)
-console.log(arr1)
+console.log(arr1);
+// for each loops
+
+arr1.forEach((element)=> {
+    console.log(element*element)
+})
+
+let name = 'farhan';
+let a = Array.from(name);
+console.log(a);
+for (let i of a){
+    console.log(i)
+}

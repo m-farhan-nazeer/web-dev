c22c34090336ccc2d7591ecd3208c00043b70731
Muhammad Farhan <farhansheikh7060@gmail.com>
2025-08-05 01:13:51 +0500

    getting element in js

---
+++ java script/script2.js
@@ -0,0 +1,7 @@
+element = document.getElementById("home");
+element.style.color = "red";
+
+Array.from(document.getElementsByTagName("li")).forEach((element )=> {
+    element.style.background = "purple";
+    
+})

ff2e46a15ac5075a89d435e16ac8f0f060c4829c
Muhammad Farhan <farhansheikh7060@gmail.com>
2025-08-04 22:44:02 +0500

    Update script.js

--- java script/script.js
+++ java script/script.js
@@ -1,13 +1,17 @@
- let a = prompt("Enter your age");
- let age = Number.parseInt(a)
 
-const candrive = (age)=>{
-    return age >= 18?true:false;
-}
 
-if (candrive(age)){
-    alert("you can drive")
-}
-else{
-    alert("you can not drive")
+const candrive = (age) => {
+  return age >= 1817 ? true : false;
+};
+let runagain = true;
+while (runagain) {
+    let a = prompt("Enter your age");
+    let age = Number.parseInt(a);
+    if (candrive(age)) {
+        alert("you can drive");
+    } else {
+        alert("you can not drive");
+    }
+    runagain = confirm("do you want to play again")
+
 }

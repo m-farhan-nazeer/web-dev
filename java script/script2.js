6eacb30fc834d5567e0928d7f59b0307e7a11473
Muhammad Farhan <farhansheikh7060@gmail.com>
2025-08-06 03:07:15 +0500

    onclick

--- java script/script2.js
+++ java script/script2.js
@@ -5,3 +5,23 @@ Array.from(document.getElementsByTagName("li")).forEach((element )=> {
     element.style.background = "purple";
     
 })
+
+const x = (event) => {
+      alert("you clicked button1");
+    };
+
+    const y = (event) => {
+      alert("you clicked button2");
+    };
+
+    const z = (event) => {
+      alert("you clicked button3");
+    };
+
+    let btn1 = document.getElementsByClassName("btn1")[0];
+    let btn2 = document.getElementsByClassName("btn2")[0];
+    let btn3 = document.getElementsByClassName("btn3")[0];
+
+    if (btn1) btn1.addEventListener("click", x);
+    if (btn2) btn2.addEventListener("click", y);
+    if (btn3) btn3.addEventListener("click", z);

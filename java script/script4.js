6f6bd12179230bd3205ec74a1961931f4f1bd846
Muhammad Farhan <farhansheikh7060@gmail.com>
2025-08-06 23:11:16 +0500

    Update script4.js

--- java script/script4.js
+++ java script/script4.js
@@ -4,24 +4,26 @@ let p1 = new Promise((resolve,reject)=>{
     console.log("promise is pending")
     setTimeout(() =>{
         console.log("promise fullfilled")
-        resolve(true)
+        reject(new Error("err"))
     },4000)
 
 })
-let p2 = new Promise((resolve,reject)=>{
-    console.log("promise is pending")
-    setTimeout(() =>{
-        console.log("rejected")
-        reject(new Error("i am an error"))
-    },5000)
+// let p2 = new Promise((resolve,reject)=>{
+//     console.log("promise is pending")
+//     setTimeout(() =>{
+//         console.log("rejected")
+//         reject(new Error("i am an error"))
+//     },5000)
 
-})
+//})
 
 p1.then((value)=>{
-    console.log(value)
+    console.log("val"+value)
+},(error)=>{
+    console.log(error)
 })
 
-p2.catch((error)=>{
-    console.log("some error has occured")
+// p2.catch((error)=>{
+//     console.log("some error has occured")
 
-})
+// })

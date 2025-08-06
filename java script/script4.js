20f3fb6db4c69813a53a7d683ea914958419688b
Muhammad Farhan <farhansheikh7060@gmail.com>
2025-08-07 02:00:08 +0500

    home page and nav bar

--- java script/script4.js
+++ java script/script4.js
@@ -1,29 +1,49 @@
 //promises
 
-let p1 = new Promise((resolve,reject)=>{
-    console.log("promise is pending")
-    setTimeout(() =>{
-        console.log("promise fullfilled")
-        reject(new Error("err"))
-    },4000)
-
-})
-// let p2 = new Promise((resolve,reject)=>{
+// let p1 = new Promise((resolve,reject)=>{
 //     console.log("promise is pending")
 //     setTimeout(() =>{
-//         console.log("rejected")
-//         reject(new Error("i am an error"))
-//     },5000)
+//         console.log("promise fullfilled")
+//         reject(new Error("err"))
+//     },4000)
 
-//})
+// })
+// // let p2 = new Promise((resolve,reject)=>{
+// //     console.log("promise is pending")
+// //     setTimeout(() =>{
+// //         console.log("rejected")
+// //         reject(new Error("i am an error"))
+// //     },5000)
 
-p1.then((value)=>{
-    console.log("val"+value)
-},(error)=>{
-    console.log(error)
-})
+// //})
+
+// p1.then((value)=>{
+//     console.log("val"+value)
+// },(error)=>{
+//     console.log(error)
+// })
 
 // p2.catch((error)=>{
 //     console.log("some error has occured")
 
 // })
+
+
+//------------Nested promises
+
+let promise1 = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        console.log("promise1")
+        resolve(true)
+    },2000)
+})
+
+promise1.then((value)=>{
+    let promise2 = new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            console.log("promise 2")
+        },2000)
+    })
+}).then((value)=>{
+    console.log('finished')
+})

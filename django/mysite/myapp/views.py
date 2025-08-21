030c0f75ff62242cf99c4fb48ffaaa56f03fd654
Muhammad Farhan <farhansheikh7060@gmail.com>
2025-08-22 03:56:25 +0500

    Update views.py

--- django/mysite/myapp/views.py
+++ django/mysite/myapp/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpRequest, HttpResponse
+
+from .models import Question
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
@@ -10,3 +12,14 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+# Leave the rest of the views (detail, results, vote) unchanged
